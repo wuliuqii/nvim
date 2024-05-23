a35d281bd5c8f96194dd4ae7846aea1bb90251e9
gallon <h1090703848@gmail.com>
2024-05-23 21:04:21 +0800

    add harpoon

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -15,6 +15,41 @@ return {
     },
   },
 
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    keys = function()
+      local keys = {
+        {
+          "<leader>H",
+          function()
+            require("harpoon"):list():add()
+          end,
+          desc = "Harpoon File",
+        },
+        {
+          "<leader>h",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Harpoon Quick Menu",
+        },
+      }
+
+      for i = 1, 5 do
+        table.insert(keys, {
+          "<leader>" .. i,
+          function()
+            require("harpoon"):list():select(i)
+          end,
+          desc = "Harpoon to File " .. i,
+        })
+      end
+      return keys
+    end,
+  },
+
   -- file explore
   {
     "stevearc/oil.nvim",
