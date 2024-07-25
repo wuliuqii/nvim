6019038b08eacc922078ad35f4251a01b6f42449
gallon <h1090703848@gmail.com>
2024-07-25 17:25:30 +0800

    update dashboard header

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,43 +1,19 @@
 return {
   "nvimdev/dashboard-nvim",
-  opts = {
-    theme = "doom",
-    hide = {
-      statusline = false,
-    },
-    config = {
-      header = {
-        [[          ____          ]],
-        [[         /.../\         ]],
-        [[        /.../--\        ]],
-        [[       /.../----\       ]],
-        [[      /.../------\      ]],
-        [[     /.../---/\---\     ]],
-        [[    /.../---/\\\---\    ]],
-        [[   /.../---/\\\\\---\   ]],
-        [[  /.../===/__\\\\\---\  ]],
-        [[ /............\\\\\---\ ]],
-        [[/..............\\\\\---\]],
-        [[\\\\\\\\\\\\\\\\\\\\\--/]],
-        [[ \\\\\\\\\\\\\\\\\\\\\/ ]],
-        [[                        ]],
-      },
-      -- stylua: ignore
-      center = {
-        { action = "Telescope fd"                     , desc = "File Browser"    , icon = " ", key = "f" },
-        { action = "Telescope oldfiles"               , desc = "Recent Files"    , icon = " ", key = "r" },
-        { action = "Lazy"                             , desc = "Lazy"            , icon = "󰒲 ", key = "l" },
-        { action = "LazyExtras"                      , desc = "Lazy Extras"     , icon = " ", key = "x" },
-        { action = 'lua require("persistence").load()', desc = "Restore Session" , icon = " ", key = "s" },
-        { action = "qa"                               , desc = "Quit"            , icon = " ", key = "q" },
-      },
-      footer = function()
-        local stats = require("lazy").stats()
-        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-        return {
-          "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
-        }
-      end,
-    },
-  },
+  opts = function(_, opts)
+    local header = {
+      "                                  ",
+      " ███▄    █ ██▒   █▓ ██▓ ███▄ ▄███▓",
+      " ██ ▀█   █▓██░   █▒▓██▒▓██▒▀█▀ ██▒",
+      "▓██  ▀█ ██▒▓██  █▒░▒██▒▓██    ▓██░",
+      "▓██▒  ▐▌██▒ ▒██ █░░░██░▒██    ▒██ ",
+      "▒██░   ▓██░  ▒▀█░  ░██░▒██▒   ░██▒",
+      "░ ▒░   ▒ ▒   ░ ▐░  ░▓  ░ ▒░   ░  ░",
+      "░ ░░   ░ ▒░  ░ ░░   ▒ ░░  ░      ░",
+      "   ░   ░ ░     ░░   ▒ ░░      ░   ",
+      "         ░      ░   ░         ░   ",
+      "               ░                  ",
+    }
+    opts.config.header = header
+  end,
 }
